ba7fa68d5d6c349c51daad1fdaa0f9445c417222
allenlin <ht016746@gmail.com>
2024-04-06 20:50:48 +0700

    Squashed commit of the following:
    commit c1c641e426d83b7999aac7a302f32b6c4ae21be5
    Author: allenlin <ht016746@gmail.com>
    Date:   Sat Apr 6 20:47:52 2024 +0700
    
        docs: credentials from bright data
    
    commit 07f836956ef1f8af9bfad4a7078fca77639e88a6
    Author: allenlin <ht016746@gmail.com>
    Date:   Sat Apr 6 20:47:41 2024 +0700
    
        feat: setup and implement scraper
    
    commit c72c4209e27db392c228d239ee8fc1d89919ca1a
    Author: allenlin <ht016746@gmail.com>
    Date:   Sat Apr 6 19:48:53 2024 +0700
    
        chore: use cheerio to scrape content

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import * as cheerio from 'cheerio';
+import { extractCurrency, extractDescription, extractPrice } from '../utils';
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
@@ -22,6 +24,63 @@ export async function scrapeAmazonProduct(url: string) {
   try {
     // Fetch the product page
     const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
+
+    // extract the product title
+    // NOTE: all css selector could be invalid due to vendor styling changes
+    const title = $('#productTitle').text().trim();
+    const currentPrice = extractPrice(
+      $('.priceToPay span.a-price-whole'),
+      $('a.size.base.a-color-price'),
+      $('.a-button-selected .a-color-base')
+    );
+
+    const originalPrice = extractPrice(
+      $('#priceblock_ourprice'),
+      $('.a-price.a-text-price span.a-offscreen'),
+      $('#listPrice'),
+      $('#priceblock_dealprice'),
+      $('.a-size-base.a-color-price')
+    );
+
+    const outOfStock =
+      $('#availability span').text().trim().toLocaleLowerCase() ===
+      'currently unavailable';
+
+    const images =
+      $('#imgBlkFront').attr('data-a-dynamic-image') ||
+      $('#landingImage').attr('data-a-dynamic-image') ||
+      '{}';
+
+    const imageUrls = Object.keys(JSON.parse(images));
+
+    const currency = extractCurrency($('.a-price-symbol'));
+
+    const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, '');
+
+    const description = extractDescription($);
+
+    // construct data object with scraped information
+    const data = {
+      url,
+      currency: currency || '$',
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice),
+      originalPrice: Number(originalPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: 'category',
+      reviewsCount: 100,
+      stars: 4.5,
+      isOutOfStock: outOfStock,
+      description,
+      lowerPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      average: Number(currentPrice) || Number(originalPrice),
+    };
+
+    return data;
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`);
   }
