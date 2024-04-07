d264233992f9babed041938f37c8f82357c488f5
allenlin <ht016746@gmail.com>
2024-04-07 20:15:52 +0700

    chore: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
     serverComponentsExternalPackages: ['mongoose']
   },
   images: {
-    domains: ['m.media-amazon.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '/**'
+      }
+    ]
   }
 };
 
