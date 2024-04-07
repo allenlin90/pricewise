02de3162e2bb8380da245dd106a4ff49b658b112
allenlin <ht016746@gmail.com>
2024-04-07 17:26:58 +0700

    chore: product details

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -4,6 +4,8 @@ import { redirect } from 'next/navigation';
 
 import { Product } from '@/types';
 import { getProductById } from '@/lib/actions';
+import { formatNumber } from '@/lib/utils';
+import PriceInfoCard from '@/components/PriceInfoCard';
 
 interface ProductDetailsProps {
   params: { id: string };
@@ -69,6 +71,79 @@ const ProductDetails = async ({ params }: ProductDetailsProps) => {
                   height={20}
                 />
               </div>
+            </div>
+          </div>
+          <div className='product-info'>
+            <div className='flex flex-col gap-2'>
+              <p className='text-[34px] text-secondary font-bold'>
+                {product.currency}{' '}
+                {formatNumber(product.currentPrice || product.originalPrice)}
+              </p>
+              <p className='text-[21px] text-black opacity-50 line-through'>
+                {product.currency} {product.originalPrice}
+              </p>
+            </div>
+            <div className='flex flex-col gap-4'>
+              <div className='flex gap-3'>
+                <div className='product-stars'>
+                  <Image
+                    src='/assets/icons/star.svg'
+                    alt='star'
+                    width={16}
+                    height={16}
+                  />
+                  <p className='text-sm text-primary-orange font-semibold'>
+                    {product.stars || '25'}
+                  </p>
+                </div>
+                <div className='product-reviews'>
+                  <Image
+                    src='/assets/icons/comment.svg'
+                    alt='comment'
+                    width={16}
+                    height={16}
+                  />
+                  <p className='text-sm text-secondary font-semibold'>
+                    {product.reviewsCount} Reviews
+                  </p>
+                </div>
+              </div>
+              <p className='text-sm text-black opacity-50'>
+                <span className='text-primary-green font-semibold'>93% </span>
+                of buyers have recommended this.
+              </p>
+            </div>
+          </div>
+          <div className='my-7 flex flex-col gap-5'>
+            <div className='flex gap-5 flex-wrap'>
+              <PriceInfoCard
+                title='Current Price'
+                iconSrc='/assets/icons/price-tag.svg'
+                value={`${product.currency} ${formatNumber(
+                  product.currentPrice || product.originalPrice
+                )}`}
+              />
+              <PriceInfoCard
+                title='Average Price'
+                iconSrc='/assets/icons/chart.svg'
+                value={`${product.currency} ${formatNumber(
+                  product.averagePrice || product.originalPrice
+                )}`}
+              />
+              <PriceInfoCard
+                title='Highest Price'
+                iconSrc='/assets/icons/arrow-up.svg'
+                value={`${product.currency} ${formatNumber(
+                  product.highestPrice || product.originalPrice
+                )}`}
+              />
+              <PriceInfoCard
+                title='Lowest Price'
+                iconSrc='/assets/icons/arrow-down.svg'
+                value={`${product.currency} ${formatNumber(
+                  product.lowestPrice || product.originalPrice
+                )}`}
+              />
             </div>
           </div>
         </div>
